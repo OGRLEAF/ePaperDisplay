d53ba19e95d5f31ee78de1b4fbfc9c31d0b2e12a
Yi Yu <yi.yu@bskyb.com>
2016-03-07 22:51:33 +0000

    Critical fixes to 'convert' command and pixel alignment calculation

--- convert_to_grayscale.sh
+++ convert_to_grayscale.sh
@@ -1,1 +1,11 @@
-convert in.png +matte -colors 4 -depth 8 -colorspace gray -resize '800x600>' pgm:-
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+    echo -n "No input file"
+    echo ""
+    exit
+else
+    IN=$1
+fi
+
+convert $IN -colorspace gray +matte -colors 4 -depth 2 -resize '800x600>' pgm:- | ./4_level_gray_4bpp_BMP_converter.py > $IN.bmp
