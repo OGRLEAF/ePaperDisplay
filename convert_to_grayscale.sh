ef95fcd2a66a398b59e4e1eeecf26a30499518dc
Yi Yu <yi.yu@bskyb.com>
2016-03-09 12:16:29 +0000

    Minor tweak

--- convert_to_grayscale.sh
+++ convert_to_grayscale.sh
@@ -8,6 +8,6 @@ else
     IN=$1
 fi
 
-convert $IN -colorspace gray +matte -colors 4 -depth 2 -resize '800x600>' pgm:- | ./4_level_gray_4bpp_BMP_converter.py > $IN.bmp
+convert $IN -colorspace gray +matte -colors 4 -depth 2 -resize '800x600>' pgm:- | ./4_level_gray_4bpp_BMP_converter.py > $IN.BMP
 
 echo "Images file names may only contain capital letters [A-Z], digits [0-9] and a total of less than 10 characters including '.BMP'"
